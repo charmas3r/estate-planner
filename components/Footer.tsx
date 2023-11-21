183c94a9e4f6d5cff423704bfc17758c6d83fc8c
Shivam <shivamwagh.1598@gmail.com>
2023-11-21 12:52:17 -0800

    Changed Text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
     return (
         <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
             <div className="grid grid-flow-col gap-4">
-                <a className="link link-hover">About the team</a>
+                <a className="link link-hover">About the team!</a>
             </div>
         </footer>
     )
