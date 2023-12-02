b3bf2bd2a1b4783a453babdd9922cdb613d75c13
Evan <evansmith0115@gmail.com>
2023-12-01 21:46:04 -0800

    landing page, create all content

--- components/ContentTable.tsx
+++ components/ContentTable.tsx
@@ -1,20 +1,17 @@
 import Image from "next/image";
-import { useRouter } from 'next/navigation'
+import {useRouter} from 'next/navigation'
+import {ContractProps} from "@/pages";
+import {TrustContractDto} from "@/data/TrustContractDto";
 
-export const ContentTable = () => {
+export const ContentTable = ({contracts}: ContractProps) => {
     const router = useRouter()
 
-    return(
+    return (
         <div className="overflow-x-auto">
             <table className="table">
                 {/* head */}
                 <thead>
                 <tr>
-                    <th>
-                        <label>
-                            <input type="checkbox" className="checkbox" />
-                        </label>
-                    </th>
                     <th>Trust name</th>
                     <th>Trust creator</th>
                     <th>Trust balance</th>
@@ -24,43 +21,40 @@ export const ContentTable = () => {
                     <th></th>
                 </tr>
                 </thead>
-                <tbody>
 
-                {/* rows */}
-                <tr>
-                    <th>
-                        <label>
-                            <input type="checkbox" className="checkbox" />
-                        </label>
-                    </th>
-                    <td>
-                        <div className="flex items-center gap-3">
-                            <div>
-                                <div className="font-bold">Evan&apos;s trust</div>
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                        0x78f6CA513132c3B6f6dCcA0e7A6C40B9d05F298a
-                    </td>
-                    <td>
-                        10.512312
-                        <br/>
-                        <span className="badge badge-ghost badge-sm">Eth</span>
-                    </td>
-                    <td><span className="badge badge-success badge-sm">Active</span></td>
-                    <th>
-                        <button className="btn btn-ghost btn-xs rounded-xl" onClick={() => router.push(`/details/0x1234}`)}>details</button>
-                    </th>
-                    <th>
-                        <button className="btn btn-ghost btn-xs rounded-xl" >edit</button>
-                    </th>
-                    <th>
-                        <button className="btn btn-ghost btn-circle">
-                            <Image src="/trash-icon.svg" height={24} width={24}  alt={"trash"}/>
-                        </button>
-                    </th>
-                </tr>
+                <tbody>
+                {contracts.map((item) => {
+                    return (
+                        <tr key={item.name}>
+                            <td>
+                                <div className="flex items-center gap-3">
+                                    <div>
+                                        <div className="font-bold">{item.name}</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td> {item.trustor} </td>
+                            <td> {item.balance.toString()}
+                                <br/>
+                                <span className="badge badge-ghost badge-sm">Eth</span>
+                            </td>
+                            <td><span className="badge badge-success badge-sm">Active</span></td>
+                            <th>
+                                <button className="btn btn-ghost btn-xs rounded-xl"
+                                        onClick={() => router.push(`/details/0x1234}`)}>details
+                                </button>
+                            </th>
+                            <th>
+                                <button className="btn btn-ghost btn-xs rounded-xl">edit</button>
+                            </th>
+                            <th>
+                                <button className="btn btn-ghost btn-circle">
+                                    <Image src="/trash-icon.svg" height={24} width={24} alt={"trash"}/>
+                                </button>
+                            </th>
+                        </tr>
+                    )
+                })}
                 </tbody>
             </table>
         </div>
--- data/TrustContractDto.ts
+++ data/TrustContractDto.ts
@@ -1,6 +1,22 @@
 export type TrustContractDto = {
     name: string,
     balance: bigint,
-    creator: string,
-    active: boolean,
+    trustor: string,
 }
+
+export class TrustContractDtoImpl implements TrustContractDto{
+    public name: string;
+    public balance: bigint;
+    public trustor: string;
+
+    public constructor(
+        trustor: string,
+        balance: bigint,
+        name: string,
+    ) {
+        this.name = name;
+        this.balance = balance;
+        this.trustor = trustor;
+    }
+}
+
