030b557e0a9572602c9f4464f2b8c43066838b1f
Evan <evansmith0115@gmail.com>
2023-12-01 22:42:13 -0800

    vercel test deployment

--- ethereum/contract.ts
+++ ethereum/contract.ts
@@ -1,3 +1,3 @@
-import EstatePlanning from "../../estate-planner/ethereum/build/EstatePlanning.json";
+import EstatePlanning from "../ethereum/build/EstatePlanning.json";
 
 export const contractAbi = EstatePlanning.abi
--- ethereum/factory.ts
+++ ethereum/factory.ts
@@ -1,4 +1,4 @@
-import EstatePlanningFactory from "../../estate-planner/ethereum/build/EstatePlanningFactory.json";
+import EstatePlanningFactory from "../ethereum/build/EstatePlanningFactory.json";
 
 export const factoryAddress = "0x6c0d404f981253CBc31c649E2A5d6C4af4F4D683"
 export const factoryAbi = EstatePlanningFactory.abi
