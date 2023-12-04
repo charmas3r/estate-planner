80fa1354340f5b7079e46a7ceb6e62d894bead98
Evan <evansmith0115@gmail.com>
2023-12-03 21:35:25 -0800

    searching functionality finished

--- components/SearchBar.tsx
+++
@@ -1,5 +0,0 @@
-export const SearchBar = () => {
-    return(
-        <input type="text" placeholder="Search for trust address" className="input text-slate-400 w-full max-w-md rounded-2xl" />
-    )
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import {Footer} from "@/components/Footer";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Web3} from "web3";
-import {SearchBar} from "@/components/SearchBar";
 import {ContentTable} from "@/components/ContentTable";
 import {CreateButton} from "@/components/CreateButton";
 import {getTrusts} from "@/data/EstatePlanningFactoryApi";
@@ -21,14 +20,27 @@ export default function Home() {
         = useState<PrimaryAccount>({account: ''});
     const [contracts, setContracts]
         = useState<TrustContractDto[]>([]);
+    const [searchContract, setSearchContract]
+        = useState<TrustContractDto>()
 
     let accounts;
     let primary: PrimaryAccount = new class implements PrimaryAccount {
         account: string;
     };
-
     let factoryContractProps: FactoryContractProps = new class implements FactoryContractProps {
         contracts: TrustContractDto[];
+    }
+
+    function onSearchAction(searchAddress: string) {
+        if (searchAddress.length > 0) {
+            contracts.map((item) => {
+                if (item.address == searchAddress) {
+                    setSearchContract(item)
+                }
+            });
+        } else {
+            setSearchContract(null)
+        }
     }
 
     useEffect(() => {
@@ -91,7 +103,47 @@ export default function Home() {
             .catch(console.error);
     }, []);
 
-    return (
+    if (searchContract == null) {
+        return (
+            <div className="bg-base-100">
+                <main>
+                    <div
+                        className="from-base-100 to-secondary text-primary-content -mt-[4rem] grid place-items-center items-end bg-gradient-to-br pt-16">
+                        <div className="hero mx-4 h-full w-full bg-fixed min-h-screen">
+                            <div className="hero-overlay bg-opacity-20">
+                                <h1 className="text-xl md:text-2xl lg:text-3xl font-bold font text-center mt-5 lg:mt-20 mr-5 ml-5">Estate
+                                    Planning dApp</h1>
+                            </div>
+                            <div className="hero-content text-center text-neutral-content mt-40">
+                                <div className="grid grid-flow-row-dense grid-cols-3 grid-rows-3">
+                                    <div className="col-span-2">
+                                        <div className="form-control">
+                                            <div className="input-group">
+                                                <input type="search"
+                                                       placeholder="Search for trust address"
+                                                       className="input text-slate-400 w-full max-w-md rounded-2xl"
+                                                       onChange={(e) => {
+                                                           onSearchAction(e.target.value);
+                                                       }}/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <CreateButton/>
+                                    </div>
+                                    <div className="col-span-3">
+                                        <ContentTable contracts={contracts}/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <Footer/>
+            </div>
+        )
+    } else {
+        return (
         <div className="bg-base-100">
             <main>
                 <div
@@ -102,15 +154,42 @@ export default function Home() {
                                 Planning dApp</h1>
                         </div>
                         <div className="hero-content text-center text-neutral-content mt-40">
-                            <div className="grid grid-flow-row-dense grid-cols-3 grid-rows-3">
+
+                            <div className="grid grid-flow-row-dense grid-cols-3 grid-rows-1">
                                 <div className="col-span-2">
-                                    <SearchBar/>
+                                    <div className="form-control">
+                                        <div className="input-group">
+                                            <input type="search"
+                                                   placeholder="Search for trust address"
+                                                   className="input text-slate-400 w-full max-w-md rounded-2xl"
+                                                   onChange={(e) => {
+                                                       onSearchAction(e.target.value);
+                                                   }}/>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div>
                                     <CreateButton/>
                                 </div>
                                 <div className="col-span-3">
-                                    <ContentTable contracts={contracts}/>
+                                    <div>
+                                        <h2 className="mt-20 mb-2 text-4xl font-extrabold md:text-6xl">Contract found</h2>
+                                        <br/>
+                                    </div>
+                                    <div className="card glass bg-opacity-60 text-primary-content m-auto">
+                                        <div className="card-body">
+                                            <h1 className="mb-5 text-5xl font-bold card-title">{searchContract?.name}</h1>
+                                            <h5 className="text-xl card-title">Contract address:</h5>
+                                            <ul className="list-none">
+                                                <li>{searchContract?.address}</li>
+                                            </ul>
+                                            <br/>
+                                            <div className="card-actions justify-end">
+                                                <button className="btn btn-primary">Go to details</button>
+                                                <button className="btn btn-warning">Edit this contract</button>                                            </div>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
@@ -119,7 +198,8 @@ export default function Home() {
             </main>
             <Footer/>
         </div>
-    )
+        )
+    }
 }
 
 
