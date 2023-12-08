91913cdee0f925ac240677f7d859b3a27bbc8e7d
Yasameen Murad <murad005@csusm.edu>
2023-12-08 12:35:55 -0800

    The Contract

--- ethereum/contracts/EstatePlanning.sol
+++ ethereum/contracts/EstatePlanning.sol
@@ -1,9 +1,8 @@
 //SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 contract EstatePlanningFactory {
-    address admin; // only one admin
+    address admin;
     EstatePlanning[] public deployedContracts;
     address[] users;
 
@@ -21,14 +20,12 @@ contract EstatePlanningFactory {
     }
 
     function getDeployedTrustContract()
-    public
-    view
-    returns (EstatePlanning[] memory)
+        public
+        view
+        returns (EstatePlanning[] memory)
     {
         return deployedContracts;
     }
-
-    //getters and setters for admin
 
     function getAdmin() public view returns (address) {
         return admin;
@@ -41,39 +38,84 @@ contract EstatePlanningFactory {
 }
 
 contract EstatePlanning {
-    //the contracts block
-
     //The variables
 
-    address admin; // only one admin
+    address admin;
     address[] users;
-
     address public trustor;
-
     address[] trustees;
-
     mapping(address => uint256) public beneficiariesPercentage;
-    address[] beneficiaries; //new change created the array of beneficiaries
-
+    address[] beneficiaries;
     uint256 totalAmount;
-
-    string trustName; //keyword
-
+    string trustName;
     bool activeStatus = true;
-
     bool isRevoked;
-
-    mapping(address => bool) activeTrustor;
-
     uint256[] percentages;
 
     //the functions
+
+    constructor(
+        string memory _trustName,
+        address _admin,
+        address[] memory _users
+    ) payable {
+        admin = _admin;
+        trustName = _trustName;
+        users = _users;
+    }
+
+    function setTrustor(address _trustor) public payable requireAdminOnly {
+        trustor = _trustor;
+    }
+
+    function setTrustees(address[] memory trustee)
+        public
+        payable
+        requireTrustorOnly
+    {
+        trustees = trustee;
+    }
+
+    function setBeneficiaries(address beneficiary, uint256 num)
+        public
+        requireTrustorOnly
+    {
+        beneficiariesPercentage[beneficiary] = num;
+        beneficiaries.push(beneficiary);
+        percentages.push(num);
+    }
 
     function setRevoked(bool _isRevoked) public requireTrustorOnly {
         isRevoked = _isRevoked;
     }
 
-    //This is the new pay function
+    function getTrustor()
+        public
+        view
+        requireAdminTrustorOnly
+        returns (address)
+    {
+        return trustor;
+    }
+
+    function getTrustees()
+        public
+        view
+        requireAdminTrustorOnly
+        returns (address[] memory)
+    {
+        return trustees;
+    }
+
+    function getBeneficiaries()
+        public
+        view
+        requireAdminTrustorOnly
+        returns (address[] memory)
+    {
+        return beneficiaries;
+    }
+
     function payoutTrust() public requireAdminTrustorOnly {
         require(isRevoked == true);
         uint256 arrayLength = beneficiaries.length;
@@ -89,106 +131,37 @@ contract EstatePlanning {
         }
     }
 
-    modifier requireAdminTrustorOnly() {
-        require(msg.sender == admin || msg.sender == trustor); // admin and trustor both
-        _;
-    }
-
-    //The constructor
-
-    constructor(
-        string memory _trustName,
-        address _admin,
-        address[] memory _users
-    ) payable {
-        admin = _admin;
-        trustName = _trustName;
-        users = _users;
-    }
-
     function enter(
         address[] memory _beneficiaries,
         uint256[] memory _percentages,
         address[] memory _trustees
-    ) public requireAuthenticatedUsers payable {
+    ) public payable requireAuthenticatedUsers {
         trustor = msg.sender;
-        totalAmount = msg.value; //trust amount
+        totalAmount = msg.value;
         beneficiaries = _beneficiaries;
         percentages = _percentages;
         trustees = _trustees;
     }
 
-    //the destructor
-
     function deleteContract() public requireTrustorOnly {
-        selfdestruct(payable(trustor)); //typecast to payable bcuz not supported after 0.8.0
-        activeStatus = false; //The contract is no longer active
+        selfdestruct(payable(trustor));
+        activeStatus = false;
     }
 
-    //getters and setters for trustor
-
-    function setTrustor(address _trustor) public requireAdminOnly payable {
-        trustor = _trustor;
-    }
-
-    function getTrustor()
-    public
-    view
-    requireAdminTrustorOnly
-    returns (address)
-    {
-        return trustor;
-    }
-
-    //getters for trustees
-
-    function getTrustees()
-    public
-    view
-    requireAdminTrustorOnly
-    returns (address[] memory)
-    {
-        return trustees;
-    }
-
-    //getters and setters for beneficiaries
-
-    function setBeneficiaries(address beneficiary, uint256 num)
-    public
-    requireTrustorOnly
-    {
-        beneficiariesPercentage[beneficiary] = num;
-        beneficiaries.push(beneficiary);
-        percentages.push(num);
-    }
-
-    function getBeneficiaries()
-    public
-    view
-    requireAdminTrustorOnly
-    returns (address[] memory)
-    {
-        return beneficiaries;
-    }
-
-    // temporary method to return some minimal trust details needed for displaying landing screen in app
-    // TODO complete this method.
-    // So far trustor, trustees, beneficiaries, totalAmount, trustName is added to be returned
     function getTrustDetails()
-    public
-    requireAuthenticatedUsers
-    requireAdminOnly
-    view
-    returns (
-        address,
-        address[] memory,
-        uint256,
-        string memory,
-        bool,
-        address[] memory,
-        uint256[] memory,
-        bool
-    )
+        public
+        view
+        requireAuthenticatedUsers
+        returns (
+            address,
+            address[] memory,
+            uint256,
+            string memory,
+            bool,
+            address[] memory,
+            uint256[] memory,
+            bool
+        )
     {
         return (
             trustor,
@@ -202,45 +175,36 @@ contract EstatePlanning {
         );
     }
 
-    //restricted modifiers for admin, address, amount, status not revoke
-
     modifier requireAdminOnly() {
         require(msg.sender == admin);
 
         _;
     }
 
-    //set trustee active status(restricted, contracts is in revokeable state, is actually trustor)
-
     modifier requireTrustorOnly() {
         require(msg.sender == trustor);
 
         _;
     }
-    modifier requireTrusteeTrustorOnly() {
-        for (uint i=0; i< trustees.length; i++) {
-            require((msg.sender == trustees[i] ) || msg.sender == trustor);
-        }
 
+    modifier requireAdminTrustorOnly() {
+        require(msg.sender == admin || msg.sender == trustor); // admin and trustor both
         _;
-    } //isTrusteeArray()
+    }
 
-    modifier requireAuthenticatedUsers() {
-
-        for (uint i=0; i< users.length; i++) {
-            require((msg.sender == users[i] ) || msg.sender == trustor);
+    modifier requireTrusteeTrustorOnly() {
+        for (uint256 i = 0; i < trustees.length; i++) {
+            require((msg.sender == trustees[i]) || msg.sender == trustor);
         }
 
         _;
     }
 
-    //adding trustees
+    modifier requireAuthenticatedUsers() {
+        for (uint256 i = 0; i < users.length; i++) {
+            require((msg.sender == users[i]) || msg.sender == trustor);
+        }
 
-    function setTrustees(address[] memory trustee)
-    public
-    payable
-    requireTrustorOnly
-    {
-        trustees = trustee;
+        _;
     }
 }
